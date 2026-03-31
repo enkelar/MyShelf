c900a45d6b970cc7cb42a4fe44704ef0d2f55997
enkelar <115401801+enkelar@users.noreply.github.com>
2026-03-31 13:16:46 +0200

    Update axios base URL for API requests

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://myshelf-backend.onrender.com',
+  baseURL: 'https://myshelf-i70a.onrender.com',
 });
 
 instance.interceptors.request.use((config) => {
